4c7bbf878332a58839c948c3fbd410e303385e9d
adrianpufu <adrianpufu1@gmail.com>
2016-09-20 12:27:12 +0300

    final commit

--- Scripts/js/popup.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $('.modal2 .close2').on('click', function() {
-        $(this).closest('.modal-overlay').removeClass('show');
-    });
-
-    $('.show-modal').on('click', function() {
-        $('.modal-overlay').addClass('show');
-    });
-})(jQuery);
--- Scripts/js/video-gallery.js
+++
@@ -1,3 +0,0 @@
-function cargarVideo(idframe, idvideo) {
-    document.getElementById(idframe).src = 'https://www.youtube.com/embed/' + idvideo + '?rel=0';
-}
